94e7af1de3c6350df801ae0a96bc6f95d917ea15
Baraker <barakydias@gmail.com>
2018-04-07 22:39:13 -0300

    Update Questao_5_asm.asm

--- 1_Respostas/Resposta_Aula_6_e_7/Questao_5_asm.asm
+++ 1_Respostas/Resposta_Aula_6_e_7/Questao_5_asm.asm
@@ -7,23 +7,23 @@
 
 
 Primo:		cmp.w	#0, R15
-			jz		end_primo_0
+			jz	end_primo_0
 			cmp.w	#1, R15
-			jz		end_primo_0
+			jz	end_primo_0
 			cmp.w	#2, R15
-			jz		end_primo_1
-			mov.w #2, R13			;R13 = i
-			mov.w R15, R12			;R12 guarda o valor de x
-loop_primo:	mov.w R13, R14			;R14 = i
-			mov.w R12, R15			;R15 = x, seu valor inicial
-			call  resto
-			tst   R15
-			jz	  end_primo_0
-			inc.w R13
-			cmp   R12, R13
-			jz    end_primo_1
-			jmp   loop_primo
-end_primo_0:clr.w R15
+			jz	end_primo_1
+			mov.w #2, R13		;R13 = i
+			mov.w R15, R12		;R12 guarda o valor de x
+loop_primo:		mov.w R13, R14		;R14 = i
+			mov.w 	R12, R15	;R15 = x, seu valor inicial
+			call  	resto
+			tst   	R15
+			jz	end_primo_0
+			inc.w 	R13
+			cmp   	R12, R13
+			jz    	end_primo_1
+			jmp   	loop_primo
+end_primo_0:		clr.w 	R15
 			ret
-end_primo_1:mov.w #1, R15
+end_primo_1:		mov.w 	#1, R15
 			ret
